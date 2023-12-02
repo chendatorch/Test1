41c13f693561a52c02785505dbda2985181fc627
Chenda <chendatorch@gmail.com>
2023-12-02 10:04:40 +0700

    first commit

---
+++ test01/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.name = "Show you Home Page";
+            return View();
+        }
+        //Defferent Name
+        public ActionResult ShowInfo()
+        {
+            return View("Show");
+        }
+        //In Share Folder
+        public ActionResult Detail()
+        {
+            return View("Details");
+        }
+        //Different Controller
+        public ActionResult ShowMoreDetail()
+        {
+            return View("~/Views/About/MyProfile.cshtml");
+        }
+    }
+}
